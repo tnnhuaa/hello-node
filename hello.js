c95f286cf2ca3090785f42933d8097fd013a455d
tnnhuaa <23127280@student.hcmus.edu.vn>
2025-10-07 20:54:11 +0700

    fix #2

--- hello.js
+++ hello.js
@@ -5,6 +5,5 @@ app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-const port = 3000;
-app.listen(port);
-console.log(`Server is listening on port ${port}`);
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => console.log("OK on", port));
