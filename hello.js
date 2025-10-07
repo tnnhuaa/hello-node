1537507b75ba1fc2d4b51f6df844726f7a324f25
tnnhuaa <146856535+tnnhuaa@users.noreply.github.com>
2025-10-07 13:11:11 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("Hello World from Node.js via ngrok");
+    res.send("Hello World");
 });
 
 const port = 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
+
